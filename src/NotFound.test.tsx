617904a86abe434036dfba7f97453cfc7bb31886
KG <acrkdduf@naver.com>
2021-09-11 23:20:36 +0900

    파라그래프 렌더링 테스트 로직 작성

--- src/NotFound.test.tsx
+++ src/NotFound.test.tsx
@@ -7,4 +7,10 @@ describe("<NotFound />", () => {
     const header = getByText("Page Not Found");
     expect(header).toBeInTheDocument();
   });
+
+  it("render paragraph", () => {
+    const { getByText } = render(<NotFound />);
+    const paragraph = getByText(/^해당 페이지/);
+    expect(paragraph).toHaveTextContent("해당 페이지를 찾을 수가 없습니다.");
+  });
 });
