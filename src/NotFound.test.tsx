8213edf7c6d47ca24d3b040bde8c16bd7ae3db41
KG <acrkdduf@naver.com>
2021-09-11 23:22:05 +0900

    이미지 렌더링 테스트 로직 작성

--- src/NotFound.test.tsx
+++ src/NotFound.test.tsx
@@ -13,4 +13,13 @@ describe("<NotFound />", () => {
     const paragraph = getByText(/^해당 페이지/);
     expect(paragraph).toHaveTextContent("해당 페이지를 찾을 수가 없습니다.");
   });
+
+  it("render image", () => {
+    const { getByAltText } = render(<NotFound />);
+    const image = getByAltText("404");
+    expect(image).toHaveAttribute(
+      "src",
+      "https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif"
+    );
+  });
 });
