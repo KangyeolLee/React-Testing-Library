4af3aba9c20466b035bd7640bcc742ca003eda6e
KG <acrkdduf@naver.com>
2021-09-13 22:32:17 +0900

    변경된 파일 구조에 따른 참조 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import App from "./layouts/App";
 
 ReactDOM.render(
   <React.StrictMode>
