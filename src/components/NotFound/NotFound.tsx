fa5b0eab719b6f0c52693146d778f7459c5b8ea1
KG <acrkdduf@naver.com>
2021-09-13 22:31:48 +0900

    컴포넌트 계층 구조 분리

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h2>Page Not Found</h2>
+      <p>해당 페이지를 찾을 수가 없습니다.</p>
+      <img
+        alt="404"
+        src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
