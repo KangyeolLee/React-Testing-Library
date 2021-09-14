6546e18859c4076d25ad38efd1e9ecf07c322a84
KG <acrkdduf@naver.com>
2021-09-14 21:45:34 +0900

    동작 컴포넌트 테스팅을 위한 구조 작성

---
+++ src/components/SubmitForm/SubmitForm.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const SubmitForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <form>
+      <label htmlFor="email">
+        이메일
+        <input
+          name="email"
+          type="email"
+          placeholder="이메일 입력"
+          value={email}
+          onChange={(e) => handleOnChangeInput(e)}
+        />
+      </label>
+
+      <label htmlFor="password">
+        비밀번호
+        <input
+          name="password"
+          type="password"
+          placeholder="비밀번호 입력"
+          value={password}
+          onChange={(e) => handleOnChangeInput(e)}
+        />
+      </label>
+
+      <button disabled={!email || !password}>로그인</button>
+    </form>
+  );
+};
+
+export default SubmitForm;
