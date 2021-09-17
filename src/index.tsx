41c4a63bb7df67f526cd37f572ead5e23d7f6061
KG <acrkdduf@naver.com>
2021-09-17 22:39:19 +0900

    수정된 파일 참조 경로 반영 및 구조 변경으로 인한 불필요 파일 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
 import App from "./layouts/App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
