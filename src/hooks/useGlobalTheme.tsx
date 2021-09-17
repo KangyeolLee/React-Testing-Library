cf69fb893921553d4f3795067fd3f865f9627d0b
KG <acrkdduf@naver.com>
2021-09-18 00:56:55 +0900

    GlobalTheme 관련 hooks 작성

---
+++ src/hooks/useGlobalTheme.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { lightMode, darkMode } from "../styles/theme";
+import { THEME } from "../constants";
+
+const useGlobalTheme = () => {
+  const localTheme =
+    localStorage.getItem("theme") === THEME.dark ? THEME.dark : THEME.light;
+  const [theme, setTheme] = useState(localTheme);
+
+  const themeMode = theme === THEME.dark ? darkMode : lightMode;
+
+  const toggleMode = () => {
+    const _theme = theme === THEME.dark ? THEME.light : THEME.dark;
+    setTheme(_theme);
+    localStorage.setItem("theme", _theme);
+  };
+
+  return [themeMode, toggleMode] as const;
+};
+
+export default useGlobalTheme;
