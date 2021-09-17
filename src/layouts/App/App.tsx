26c56ed81b614a7e6a5bda3d5a8cb5bbe8f6942b
KG <acrkdduf@naver.com>
2021-09-18 00:58:49 +0900

    App 컴포넌트 themeProvider 적용 및 theme 토글 버튼 작성

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -1,10 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import useGlobalTheme from "../../hooks/useGlobalTheme";
 import Home from "../../pages/Home";
+import * as S from "./styles";
 
 function App() {
+  const [theme, setTheme] = useGlobalTheme();
+
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <ThemeProvider theme={theme}>
+      <S.App>
+        <button onClick={setTheme}>테마변경</button>
+        <Home />
+      </S.App>
+    </ThemeProvider>
   );
 }
 
