fb3a8ac374025bbed90f417bc85f31021a14a66e
KG <acrkdduf@naver.com>
2021-09-18 01:04:08 +0900

    테마 토글버튼 추가로 인한 관련 테스트 파일 작성

--- src/layouts/App/App.test.tsx
+++ src/layouts/App/App.test.tsx
@@ -1,9 +1,21 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 describe("<App />", () => {
   it("render whole component", () => {
     const { container } = render(<App />);
     expect(container).toBeInTheDocument();
+  });
+
+  it("toggle button for changing global theme", () => {
+    const { getByText } = render(<App />);
+
+    const button = getByText("테마변경");
+
+    expect(button).toBeEnabled();
+
+    fireEvent.click(button);
+
+    expect(button).toBeEnabled();
   });
 });
