a4d3f9a41006f800860986eeef6ca4fdd9e1a1d2
KG <acrkdduf@naver.com>
2021-09-18 23:33:53 +0900

    App 전체 화면 비율 차지하도록 스타일 설정

--- src/layouts/App/styles.ts
+++ src/layouts/App/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const App = styled.div`
   background-color: ${({ theme }) => theme.color.background};
-  max-width: 108rem;
-  margin: auto;
-  padding: 0 2rem;
+  width: 100vw;
+  height: 100vh;
 `;
