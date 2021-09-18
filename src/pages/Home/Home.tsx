1f265a8e5d86d3bb719a47c8c04c6c50c73e8f36
KG <acrkdduf@naver.com>
2021-09-18 23:28:41 +0900

    Home 컴포넌트 디자인 수정 및 적용

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
-import SubmitForm from "@/components/SubmitForm";
 import * as S from "./styles";
 
 const Home = () => {
   return (
     <S.Home>
-      <SubmitForm />
+      <S.HomeCard bgColor="#ff791e">폼 제출</S.HomeCard>
+      <S.HomeCard bgColor="#f4b400">데이터 불러오기</S.HomeCard>
+      <S.HomeCard bgColor="#1e48ff">잘못된 페이지 요청</S.HomeCard>
     </S.Home>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,29 @@
 import styled from "styled-components";
 
+type CardProps = {
+  bgColor: string;
+};
+
 export const Home = styled.div`
   display: flex;
-  flex-direction: column;
   align-items: center;
+  justify-content: center;
+  gap: 1rem;
+`;
+
+export const HomeCard = styled.section<CardProps>`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
   padding: 2rem;
+  border-radius: 1rem;
+  background-color: ${({ bgColor }) => bgColor};
+  width: 33%;
+  max-width: 20rem;
+  height: 30rem;
+  box-sizing: border-box;
+  font-family: "Nanum Gothic", sans-serif;
+  font-weight: 700;
+  font-size: 1.5rem;
+  color: #fff;
 `;
