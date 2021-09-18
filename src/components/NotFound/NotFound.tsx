c288a0720518ead31456db4f692d85db31712eac
KG <acrkdduf@naver.com>
2021-09-18 23:28:26 +0900

    NotFound 컴포넌트 디자인 수정 및 적용

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,13 +1,15 @@
+import * as S from "./styles";
+
 const NotFound = () => {
   return (
-    <div>
+    <S.NotFound>
       <h2>Page Not Found</h2>
       <p>해당 페이지를 찾을 수가 없습니다.</p>
       <img
         alt="404"
         src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif"
       />
-    </div>
+    </S.NotFound>
   );
 };
 
