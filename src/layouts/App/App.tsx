c594d88b7c5a72da20f3e1a284bd3b34be19371f
KG <acrkdduf@naver.com>
2021-09-19 00:04:04 +0900

    App 모드 체인저 버튼 디자인 수정 및 적용

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -2,15 +2,20 @@ import { ThemeProvider } from "styled-components";
 import useGlobalTheme from "@/hooks/useGlobalTheme";
 import Home from "@/pages/Home";
 import * as S from "./styles";
+import { ReactComponent as DarkModeIcon } from "@/assets/darkmode.svg";
+import { ReactComponent as LightModeIcon } from "@/assets/lightmode.svg";
+import { THEME } from "@/constants/styles";
 
 function App() {
-  const [theme, setTheme] = useGlobalTheme();
+  const [theme, setTheme, themeString] = useGlobalTheme();
 
   return (
     <ThemeProvider theme={theme}>
       <S.App>
-        <button onClick={setTheme}>테마변경</button>
         <Home />
+        <S.ThemeChanger className="touchable" onClick={setTheme}>
+          {themeString === THEME.dark ? <DarkModeIcon /> : <LightModeIcon />}
+        </S.ThemeChanger>
       </S.App>
     </ThemeProvider>
   );
--- src/layouts/App/styles.ts
+++ src/layouts/App/styles.ts
@@ -1,7 +1,32 @@
 import styled from "styled-components";
 
 export const App = styled.div`
+  position: relative;
   background-color: ${({ theme }) => theme.color.background};
+  padding: 3rem;
+  box-sizing: border-box;
   width: 100vw;
   height: 100vh;
+  transition: background-color 0.25s ease-in-out;
+`;
+
+export const ThemeChanger = styled.button`
+  position: absolute;
+  bottom: 1rem;
+  right: 1rem;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 3rem;
+  border: none;
+  outline: none;
+  background-color: ${({ theme }) => theme.color.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    width: 25px;
+    height: 25px;
+    stroke: ${({ theme }) => theme.color.background};
+  }
 `;
