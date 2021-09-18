6d657064d1c9bafe54e6d68083fe6718604a6100
KG <acrkdduf@naver.com>
2021-09-18 23:34:20 +0900

    공통 클래스 container 작성 : 최대 1080px 해상도를 가진 컨테이너 클래스

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -3,6 +3,12 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  .container {
+    max-width: 1080px;
+    margin: auto;
+    padding: 0 2rem;
+  }
 `;
 
 export default GlobalStyle;
