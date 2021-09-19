1637770841429665541ad3dbbd6b7c677bab2eda
KG <acrkdduf@naver.com>
2021-09-19 22:01:53 +0900

    App 컴포넌트 라우터 경로 추가

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -1,11 +1,16 @@
+import * as S from "./styles";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { ReactComponent as DarkModeIcon } from "@/assets/darkmode.svg";
+import Home from "@/pages/Home";
+import { ReactComponent as LightModeIcon } from "@/assets/lightmode.svg";
+import LoadData from "@/components/LoadData";
+import NotFound from "@/components/NotFound";
+import SubmitForm from "@/components/SubmitForm/SubmitForm";
+import { THEME } from "@/constants/styles";
 import { ThemeProvider } from "styled-components";
 import useGlobalTheme from "@/hooks/useGlobalTheme";
-import Home from "@/pages/Home";
-import * as S from "./styles";
-import { ReactComponent as DarkModeIcon } from "@/assets/darkmode.svg";
-import { ReactComponent as LightModeIcon } from "@/assets/lightmode.svg";
-import { THEME } from "@/constants/styles";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   const [theme, setTheme, themeString] = useGlobalTheme();
@@ -16,6 +21,9 @@ function App() {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/submitForm" component={SubmitForm} />
+            <Route path="/loadData" component={LoadData} />
+            <Route component={NotFound} />
           </Switch>
         </BrowserRouter>
         <S.ThemeChanger className="touchable" onClick={setTheme}>
