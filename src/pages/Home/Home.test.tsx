5f2471e48ab7cc356f4a29da939fcdced76b44cb
KG <acrkdduf@naver.com>
2021-09-20 22:01:51 +0900

    Home 컴포넌트 테스트 파일 작성 및 이를 위한 data-testid 정의

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,9 +1,55 @@
-import { render } from "@testing-library/react";
 import Home from "./Home";
+import box from "@/assets/box.png";
+import car from "@/assets/car.png";
+import error from "@/assets/404error.png";
+import { lightMode } from "@/styles/theme";
+import renderComponent from "@/utils/test/renderComponent";
 
 describe("<Home />", () => {
   it("render whole component", () => {
-    const { container } = render(<Home />);
+    const { container } = renderComponent({
+      theme: lightMode,
+      JSXElement: Home,
+    });
     expect(container).toBeInTheDocument();
+  });
+
+  it("render form card", () => {
+    const { getByText, getByAltText } = renderComponent({
+      theme: lightMode,
+      JSXElement: Home,
+    });
+
+    const title = getByText(/폼 제출/);
+    expect(title).toBeInTheDocument();
+
+    const image = getByAltText(/폼 제출 이미지/);
+    expect(image).toHaveAttribute("src", box);
+  });
+
+  it("render data card", () => {
+    const { getByText, getByAltText } = renderComponent({
+      theme: lightMode,
+      JSXElement: Home,
+    });
+
+    const title = getByText(/데이터 불러오기/);
+    expect(title).toBeInTheDocument();
+
+    const image = getByAltText(/데이터 불러오기 이미지/);
+    expect(image).toHaveAttribute("src", car);
+  });
+
+  it("render error card", () => {
+    const { getByText, getByAltText } = renderComponent({
+      theme: lightMode,
+      JSXElement: Home,
+    });
+
+    const title = getByText(/잘못된 페이지 요청/);
+    expect(title).toBeInTheDocument();
+
+    const image = getByAltText(/잘못된 페이지 요청 이미지/);
+    expect(image).toHaveAttribute("src", error);
   });
 });
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,42 +10,46 @@ const Home = () => {
     <S.Home className="container">
       <h1>React Testing Lib</h1>
       <div className="card-wrapper">
-        <Link to="/submitForm">
+        <Link to="/submitForm" data-testid="form-link">
           <S.HomeCard bgColor="#ff791e">
             <span className="article-title">폼 제출</span>
-            <S.HiddenContent>
+            <S.HiddenContent data-testid="form-hidden-test">
               <span>폼 관련 데이터를 입력하고</span>
               <span>버튼 유효성 검사 및</span>
               <span>제출 관련 이벤트를</span>
               <span>테스트합니다.</span>
             </S.HiddenContent>
-            <img src={box} alt="폼 제출 이미지" />
+            <img draggable="false" src={box} alt="폼 제출 이미지" />
           </S.HomeCard>
         </Link>
 
-        <Link to="/loadData">
+        <Link to="/loadData" data-testid="data-link">
           <S.HomeCard bgColor="#f4b400">
             <span className="article-title">데이터 불러오기</span>
-            <S.HiddenContent>
+            <S.HiddenContent data-testid="data-hidden-test">
               <span>Suspense를 이용하여</span>
               <span>데이터를 불러오고</span>
               <span>로딩/에러처리 관련 내용을</span>
               <span>테스트합니다.</span>
             </S.HiddenContent>
-            <img src={car} alt="데이터 불러오기 이미지" />
+            <img draggable="false" src={car} alt="데이터 불러오기 이미지" />
           </S.HomeCard>
         </Link>
 
-        <Link to="/notFound">
+        <Link to="/notFound" data-testid="error-link">
           <S.HomeCard bgColor="#1e48ff">
             <span className="article-title">잘못된 페이지 요청</span>
-            <S.HiddenContent>
+            <S.HiddenContent data-testid="error-hidden-test">
               <span>경로에 맞지 않는 페이지</span>
               <span>또는 잘못된 요청이 올 경우</span>
               <span>출력하는 페이지를</span>
               <span>테스트합니다.</span>
             </S.HiddenContent>
-            <img src={error} alt="잘못된 페이지 요청 이미지" />
+            <img
+              draggable="false"
+              src={error}
+              alt="잘못된 페이지 요청 이미지"
+            />
           </S.HomeCard>
         </Link>
       </div>
