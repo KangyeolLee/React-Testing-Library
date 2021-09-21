cee9416bd0f91bda29a2bc67671b6a36d8cf1450
KG <acrkdduf@naver.com>
2021-09-22 00:30:13 +0900

    github api 일일 limitation 으로 토큰을 이용해 인증 계정으로 호출하도록 header 정보 추가

--- src/api/fetchers.ts
+++ src/api/fetchers.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const fetcherUserByPage = async (url: string) => {
-  const res = await axios.get(url);
+  const res = await axios.get(url, {
+    headers: {
+      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
+  });
   return res.data;
 };
